63886e2179ec35c4213ff67e82a43b0ccf90e890
zeled3813 <zeled3813@hstat.org>
2017-10-02 15:49:29 +0000

    rspec done

--- sweet_ride.rb
+++ sweet_ride.rb
@@ -1,1 +1,20 @@
 #code your answers here
+def check_it_out
+    return "Check out my sweet new ride, dude!"
+end
+
+def no_big_deal(car_name)
+    return "No big deal, it's a #{car_name}"
+end
+
+def stolen
+    return "dude, where's my car?"
+end
+
+def reporting_theft(car)
+    return "OFFICER, SOMEONE'S STOLEN MY #{car.upcase}"
+end
+
+def how_long_until_i_can_get_another_car(car_price,hourly_wage)
+    car_price/hourly_wage
+end
